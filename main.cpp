358082e06e2261ef5033340f08888a50cb5afef5
JeromeChallet <jeromepchallet@gmail.com>
2026-05-28 21:20:00 +0900

    rules

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+/*
+1. the player is allowed 8 mistakes
+2. the answer comes from a group of predefined random words
+3. tell the player how many incorrect guesses he has left if done a mistake
+4. constantly show the player the word he is guessing
+5. tell the player the letter has already typed in it happens
+6. give the option to play again or end the game at any time
+*/
+
 int main(){
     char guessLetter;
 
