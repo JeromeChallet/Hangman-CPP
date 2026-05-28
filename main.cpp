5d324fd5f317b725fb77db6ff2c596ff01572134
JeromeChallet <jeromepchallet@gmail.com>
2026-05-28 21:10:09 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <iterator>
+
+using namespace std;
+
+int main(){
+    char guessLetter;
+
+    cout << "Welcome to Hangman. Good Luck!\n";
+
+    cout << "Enter your guess: ";
+    cin >> guessLetter;
+
+    return 0;
+}
